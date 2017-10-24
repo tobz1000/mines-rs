8815678500551b51a397986f67824f587c6d5136
Toby Dimmick <tobydimmick@protonmail.ch>
2017-10-24 21:51:21 +0100

    Use wrapper try-able main method

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
 #[macro_use]
 extern crate serde_derive;
+
+extern crate serde_json;
 extern crate tokio_core;
 
 mod server_wrapper;
 
 use self::tokio_core::reactor;
 use server_wrapper::{JsonServerWrapper};
+use std::error::Error;
 
 fn main() {
-	let mut event_loop_core = reactor::Core::new().unwrap();
+	main_try().unwrap();
+}
+
+fn main_try() -> Result<(), Box<Error>> {
+	let mut event_loop_core = reactor::Core::new()?;
 	let server = JsonServerWrapper::new_game(
 		vec![10, 10], 10, None, &mut event_loop_core
-	).unwrap();
-	println!("{}", server.status().id);
+	)?;
+	println!("{}", serde_json::to_string(server.status())?);
+
+	Ok(())
 }
