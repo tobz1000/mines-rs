7d0c175c91e8dd63923166f65b0490aad94e2e9f
Toby Dimmick <tobydimmick@protonmail.ch>
2017-11-16 12:50:46 +0000

    Attempted to refactor Cell methods as GameGrid methods
    Taking the appropriate Cell as a paramter. Currently not compiling
    due to borrow issues.

--- src/client.rs
+++ src/client.rs
@@ -15,39 +15,50 @@ enum CellState {
 }
 
 struct GameGrid<'a> {
-	client: &'a Client<'a>,
 	dims: Vec<i32>,
-	arr: Vec<Option<Cell<'a>>>,
-	surr_cells: HashMap<
-		&'a Cell<'a>,
-		HashSet<&'a Cell<'a>>
-	>
+	arr: Vec<Option<Cell>>,
+	surr_cells: HashMap<&'a Cell, HashSet<&'a mut Cell>>
 }
 
 impl<'a> GameGrid<'a> {
-	pub fn new(client: &'a Client, dims: Vec<i32>) -> Self {
+	pub fn new(dims: Vec<i32>) -> Self {
 		let arr_size = dims.iter().fold(1, |a, b| a * b) as usize;
 		let mut arr: Vec<Option<Cell>> = (0..arr_size).map(|_| None).collect();
-		let mut surr_cells = HashMap::new();
+		let mut surr_cells = HashMap::with_capacity(arr_size);
 
-		GameGrid { client, dims, arr, surr_cells }
+		GameGrid { dims, arr, surr_cells }
 	}
 
-	pub fn cell(&mut self, coords: Vec<i32>) -> &Cell {
-		let index = (0..coords.len()).fold(0, |acc, i| {
-			(acc * self.dims[i]) + coords[i]
-		}) as usize;
+	pub fn cell(&'a mut self, coords: Vec<i32>) -> &'a mut Cell {
+		let index = coords.iter().zip(self.dims.iter())
+			.fold(0, |acc, (&coord, &dim)| {
+				(acc * dim) + coord
+			}) as usize;
 
-		if self.arr[index] == None {
-			let cell = Cell::new(coords, self.client);
-			self.arr[index] = Some(cell);
+		match self.arr[index] {
+			Some(ref mut cell) => cell,
+			None => {
+				let cell = Cell::new(coords);
+				self.arr[index] = Some(cell);
+				&mut self.arr[index].unwrap()
+			}
 		}
-
-		self.arr[index].as_ref().unwrap()
 	}
 
-	pub fn surr_cells(&self, cell: &Cell) -> &mut Vec<&mut Cell> {
-		unimplemented!()
+	pub fn surr_cells(&'a self, cell: &'a Cell) -> &'a HashSet<&'a mut Cell> {
+		match self.surr_cells.get(cell) {
+			Some(surr_cells) => surr_cells,
+			None => {
+				let surr_cells: HashSet<&'a mut Cell> = self
+					.surr_coords(&cell.coords)
+					.into_iter()
+					.map(|coords| self.cell(coords))
+					.collect();
+
+				self.surr_cells.insert(cell, surr_cells);
+				&surr_cells
+			}
+		}
 	}
 
 	fn _surr_coords(dims: &Vec<i32>, coords: &Vec<i32>) -> Vec<Vec<i32>> {
@@ -75,6 +86,83 @@ impl<'a> GameGrid<'a> {
 	fn surr_coords(&self, coords: &Vec<i32>) -> Vec<Vec<i32>> {
 		Self::_surr_coords(&self.dims, &coords)
 	}
+
+	pub fn unknown_surr_count_mine(&self, cell: &Cell) -> i32 {
+		cell.unknown_surr_count_mine
+	}
+
+	pub fn set_unknown_surr_count_mine(&self, cell: &Cell, val: i32) {
+		if (val == 0) & (cell.state == CellState::Empty) {
+			for surr_cell in self.surr_cells(cell) {
+				if surr_cell.state == CellState::Empty {
+					surr_cell.state = CellState::ToClear;
+				}
+			}
+		}
+
+		cell.unknown_surr_count_mine = val;
+	}
+
+	pub fn unknown_surr_count_empty(&self, cell: &Cell) -> i32 {
+		match cell.unknown_surr_count_empty {
+			Some(count) => count,
+			None => {
+				let count = self.surr_cells(cell).len() as i32;
+				cell.unknown_surr_count_empty = Some(count);
+				count
+			}
+		}
+	}
+
+	pub fn set_unknown_surr_count_empty(&self, cell: &Cell, val: i32) {
+		if val == 0 {
+			for surr_cell in self.surr_cells(cell) {
+				if surr_cell.state == CellState:: Unknown {
+					surr_cell.state = CellState::Mine;
+				}
+			}
+		}
+
+		cell.unknown_surr_count_mine = val;
+	}
+}
+
+struct Client<'a> {
+	grid: GameGrid<'a>,
+}
+
+struct Cell {
+	coords: Vec<i32>,
+	state: CellState,
+	surrounding_changed: bool,
+	unknown_surr_count_mine: i32,
+	unknown_surr_count_empty: Option<i32>
+}
+
+impl Cell {
+	pub fn new(coords: Vec<i32>) -> Self {
+		Cell {
+			coords,
+			state: CellState::Unknown,
+			surrounding_changed: false,
+			unknown_surr_count_mine: 0,
+			unknown_surr_count_empty: None,
+		}
+	}
+}
+
+impl PartialEq for Cell {
+	fn eq(&self, other: &Cell) -> bool {
+		self.coords == other.coords
+	}
+}
+
+impl Eq for Cell {}
+
+impl Hash for Cell {
+	fn hash<H: Hasher>(&self, state: &mut H) {
+		self.coords.hash(state);
+	}
 }
 
 #[test]
@@ -82,7 +170,7 @@ fn test_surr_coords() {
 	let surr = GameGrid::_surr_coords(
 		&vec![10, 10],
 		&vec![9, 5]
-	); 
+	);
 
 	let expected = vec![
 		vec![8, 4],
@@ -93,72 +181,4 @@ fn test_surr_coords() {
 	];
 	
 	assert_eq!(surr, expected);
-}
-
-struct Client<'a> {
-	grid: GameGrid<'a>,
-}
-
-struct Cell<'a> {
-	coords: Vec<i32>,
-	state: CellState,
-	surrounding_changed: bool,
-	unknown_surr_count_mine: i32,
-	unknown_surr_count_empty: Option<i32>,
-	client: &'a Client<'a>
-}
-
-impl<'a> Cell<'a> {
-	pub fn new(coords: Vec<i32>, client: &'a Client) -> Self {
-		Cell {
-			coords,
-			client,
-			state: CellState::Unknown,
-			surrounding_changed: false,
-			unknown_surr_count_mine: 0,
-			unknown_surr_count_empty: None,
-		}
-	}
-
-	pub fn unknown_surr_count_mine(&self) -> i32 {
-		self.unknown_surr_count_mine
-	}
-
-	pub fn unknown_surr_count_empty(&mut self) -> i32 {
-		if self.unknown_surr_count_empty == None {
-			self.unknown_surr_count_empty = Some(
-				self.surr_cells().len() as i32
-			);
-		}
-
-		self.unknown_surr_count_empty.unwrap()
-	}
-
-	pub fn set_unknown_surr_count_empty(&mut self, val: i32) {
-		if val == 0 {
-			for cell in self.surr_cells() {
-				if cell.state == CellState::Unknown {
-					cell.state = CellState::Mine;
-				}
-			}
-		}
-	}
-
-	fn surr_cells(&self) -> &mut Vec<&mut Cell> {
-		self.client.grid.surr_cells(self)
-	}
-}
-
-impl<'a> PartialEq for Cell<'a> {
-	fn eq(&self, other: &Cell) -> bool {
-		self.coords == other.coords
-	}
-}
-
-impl<'a> Eq for Cell<'a> {}
-
-impl<'a> Hash for Cell<'a> {
-	fn hash<H: Hasher>(&self, state: &mut H) {
-		self.coords.hash(state);
-	}
 }
