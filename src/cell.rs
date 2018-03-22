052cfe3e985cd271ebd1cd55c5dab5b11fd87037
Toby Dimmick <tobydimmick@protonmail.ch>
2018-03-22 00:30:02 +0000

    Move more logic into cell.rs
    Refactor to give Cells more information about surrounding Cells. Should allow for implementation of more advanced logic.

--- src/cell.rs
+++ src/cell.rs
@@ -1,101 +1,134 @@
+use std::collections::{HashSet, VecDeque};
+
+
+#[derive(Debug)]
+pub struct CellAction {
+    pub index: usize,
+    pub action_type: CellActionType
+}
+
 #[derive(Clone, Copy, Debug)]
-pub enum CellAction {
-    IncSurrEmpty,
-    IncSurrMine,
-    ClientClear(usize),
+pub enum CellActionType {
+    MarkSurrEmpty { surr: usize },
+    MarkSurrMine { surr: usize },
+    ClientClear { mines: usize },
     ServerClear,
     Flag,
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Debug)]
+pub struct OngoingCell {
+    index: usize,
+    unknown_surr: HashSet<usize>,
+    total_surr: HashSet<usize>,
+    total_surr_mines: Option<usize>,
+    known_surr_mines: usize,
+    known_surr_empty: usize,
+}
+
+#[derive(Clone, Debug)]
 pub enum Cell {
-    Ongoing {
-        total_surr: usize,
-        total_surr_mines: Option<usize>,
-        known_surr_empty: usize,
-        known_surr_mines: usize,
-    },
-    Clear,
-    Mine
+    Ongoing(OngoingCell),
+    Complete,
 }
 
 impl Cell {
-    pub fn new(total_surr: usize) -> Self {
-        Cell::Ongoing {
-            total_surr,
+    pub fn apply_action(
+        &mut self,
+        action: CellActionType,
+        actions: &mut VecDeque<CellAction>,
+        server_to_clear: &mut HashSet<usize>,
+        server_to_flag: &mut HashSet<usize>,
+    ) {
+        let mut complete = false;
+
+        if let &mut Cell::Ongoing(ref mut ongoing) = self {
+            if let Some(action_type) = match action {
+                CellActionType::MarkSurrEmpty { surr } => {
+                    ongoing.unknown_surr.remove(&surr);
+                    ongoing.known_surr_empty += 1;
+
+                    ongoing.try_complete()
+                },
+                CellActionType::MarkSurrMine { surr } => {
+                    ongoing.unknown_surr.remove(&surr);
+                    ongoing.known_surr_mines += 1;
+
+                    ongoing.try_complete()
+                },
+                CellActionType::ClientClear { mines } => {
+                    ongoing.total_surr_mines = Some(mines);
+
+                    for &surr in ongoing.total_surr.iter() {
+                        actions.push_back(CellAction {
+                            index: surr,
+                            action_type: CellActionType::MarkSurrEmpty {
+                                surr: ongoing.index
+                            }
+                        })
+                    }
+
+                    ongoing.try_complete()
+                },
+                CellActionType::ServerClear => {
+                    if ongoing.total_surr_mines == None {
+                        server_to_clear.insert(ongoing.index);
+                    }
+
+                    None
+                },
+                CellActionType::Flag => {
+                    server_to_flag.insert(ongoing.index);
+
+                    for &surr in ongoing.total_surr.iter() {
+                        actions.push_back(CellAction {
+                            index: surr,
+                            action_type: CellActionType::MarkSurrMine {
+                                surr: ongoing.index
+                            }
+                        })
+                    }
+
+                    complete = true;
+
+                    None
+                }
+            } {
+                for &surr in ongoing.unknown_surr.iter() {
+                    actions.push_back(CellAction { index: surr, action_type });
+                }
+
+                complete = true;
+            }
+        }
+
+        if complete {
+            *self = Cell::Complete;
+        }
+    }
+
+    pub fn new(index: usize, surr_indices: HashSet<usize>) -> Self {
+        Cell::Ongoing(OngoingCell {
+            index,
+            unknown_surr: surr_indices.clone(),
+            total_surr: surr_indices,
             total_surr_mines: None,
+            known_surr_mines: 0,
             known_surr_empty: 0,
-            known_surr_mines: 0
-        }
+        })
     }
+}
 
-    pub fn to_submit(&self) -> bool {
-        match self {
-            &Cell::Ongoing {
-                total_surr_mines: None,
-                ..
-            } => true,
-            _ => false
-        }
-    }
+impl OngoingCell {
+    fn try_complete(&self) -> Option<CellActionType> {
+        if let Some(total_surr_mines) = self.total_surr_mines {
+            let unknown_mines = total_surr_mines - self.known_surr_mines;
 
-    fn try_mark_surrounding(&mut self) -> Option<CellAction> {
-        match self {
-            &mut Cell::Ongoing {
-                total_surr,
-                total_surr_mines: Some(total_surr_mines),
-                known_surr_empty,
-                known_surr_mines
-            } => {
-                if known_surr_mines == total_surr_mines {
-                    *self = Cell::Clear;
-                    Some(CellAction::ServerClear)
-                } else if known_surr_empty == total_surr - total_surr_mines {
-                    *self = Cell::Clear;
-                    Some(CellAction::Flag)
-                } else { None }
-            },
-            _ => None
-        }
-    }
-
-    pub fn set_mine(&mut self) {
-        match *self {
-            Cell::Ongoing { .. } => { *self = Cell::Mine; },
-            Cell::Mine => (),
-            Cell::Clear => panic!("Tried to flag previously-cleared cell")
-        }
-    }
-
-    pub fn set_clear(&mut self, surr_mine_count: usize) -> Option<CellAction> {
-        match *self {
-            Cell::Ongoing {
-                total_surr_mines: ref mut total_surr_mines @ None,
-                ..
-            } => {
-                *total_surr_mines = Some(surr_mine_count);
-            },
-            Cell::Ongoing { .. } => (),
-            Cell::Clear => (),
-            Cell::Mine => { panic!("Tried to clear previously-flagged cell"); }
-        }
-
-        self.try_mark_surrounding()
-    }
-
-    pub fn inc_surr_empty(&mut self) -> Option<CellAction> {
-        if let &mut Cell::Ongoing { ref mut known_surr_empty, .. } = self {
-            *known_surr_empty += 1;
-        }
-
-        self.try_mark_surrounding()
-    }
-
-    pub fn inc_surr_mine(&mut self) -> Option<CellAction> {
-        if let &mut Cell::Ongoing { ref mut known_surr_mines, .. } = self {
-            *known_surr_mines += 1;
-        }
-
-        self.try_mark_surrounding()
+            if unknown_mines == 0 {
+                Some(CellActionType::ServerClear)
+            } else if unknown_mines == self.unknown_surr.len() {
+                Some(CellActionType::Flag)
+            } else { None }
+        } else { None }
     }
 }
--- src/game_grid.rs
+++ src/game_grid.rs
@@ -3,7 +3,7 @@ use itertools::Itertools;
 use std::collections::{VecDeque, HashSet};
 use std::iter::repeat;
 use std::convert::TryInto;
-use cell::{Cell, CellAction};
+use cell::{Cell, CellAction, CellActionType};
 use server_wrapper::server_response::{CellInfo, CellState};
 
 pub type Coords = Vec<usize>;
@@ -16,16 +16,15 @@ pub struct ServerActions {
 
 pub struct GameGrid {
     dims: Coords,
-    cells: Vec<(Cell, Vec<usize>)>,
+    cells: Vec<Cell>,
 }
 
 impl GameGrid {
     pub fn new(dims: Coords) -> Self {
         let all_coords = dims.iter().map(|&d| 0..d).multi_cartesian_product();
 
-        let cells = all_coords.map(|coords| {
-            let surr_indices = surr_indices(&coords, &dims);
-            (Cell::new(surr_indices.len()), surr_indices)
+        let cells = all_coords.enumerate().map(|(i, coords)| {
+            Cell::new(i, surr_indices(&coords, &dims))
         }).collect();
 
         GameGrid { dims, cells }
@@ -44,55 +43,23 @@ impl GameGrid {
             ref coords
         } in cell_info.iter() {
             let index = coords_to_index(coords, &dims);
-            let action = match state {
-                CellState::Cleared => CellAction::ClientClear(surrounding),
-                CellState::Mine => CellAction::Flag
+            let action_type = match state {
+                CellState::Cleared => CellActionType::ClientClear {
+                    mines: surrounding
+                },
+                CellState::Mine => CellActionType::Flag
             };
-            actions.push_back((index, action));
+
+            actions.push_back(CellAction { index, action_type });
         }
 
-        while let Some((index, client_action)) = actions.pop_front() {
-            let (ref mut cell, ref surr_indices) = cells[index];
-
-            if let Some(surr_action) = match client_action {
-                CellAction::IncSurrEmpty => cell.inc_surr_empty(),
-                CellAction::IncSurrMine => cell.inc_surr_mine(),
-                CellAction::ClientClear(surr_mine_count) => {
-                    for &surr in surr_indices.iter() {
-                        actions.push_back(
-                            (surr, CellAction::IncSurrEmpty)
-                        );
-                    }
-
-                    cell.set_clear(surr_mine_count)
-                },
-                CellAction::ServerClear => {
-                    if cell.to_submit() {
-                        server_to_clear.insert(index);
-                    }
-
-                    None
-                }
-                CellAction::Flag => {
-                    if cell.to_submit() {
-                        server_to_flag.insert(index);
-
-                        for &surr in surr_indices.iter() {
-                            actions.push_back(
-                                (surr, CellAction::IncSurrMine)
-                            );
-                        }
-
-                        cell.set_mine();
-                    }
-
-                    None
-                },
-            } {
-                for &surr_index in surr_indices.iter() {
-                    actions.push_back((surr_index, surr_action));
-                }
-            }
+        while let Some(CellAction { index, action_type }) = actions.pop_front() {
+            cells[index].apply_action(
+                action_type,
+                &mut actions,
+                &mut server_to_clear,
+                &mut server_to_flag
+            );
         }
 
         let next_actions = ServerActions {
@@ -109,7 +76,7 @@ impl GameGrid {
     }
 }
 
-fn surr_indices(coords: &Coords, dims: &Coords) -> Vec<usize> {
+fn surr_indices(coords: &Coords, dims: &Coords) -> HashSet<usize> {
     let offsets = repeat(-1..=1).take(dims.len())
         .multi_cartesian_product()
         .filter(|offset| offset.iter().any(|&c| c != 0));
@@ -162,6 +129,7 @@ fn test_surr_indices() {
         (vec![0, 0], vec![1, 1], vec![]),
         (vec![1, 0], vec![2, 2], vec![0, 1, 3])
     ] {
-        assert_eq!(surr_indices(&coords, &dims), exp);
+        let exp_set = exp.into_iter().collect();
+        assert_eq!(surr_indices(&coords, &dims), exp_set);
     }
 }
