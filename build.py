21175c4e6e76dab44c208e4c07754ee1ce622bb1
Toby Dimmick <tobydimmick@protonmail.ch>
2018-09-07 22:46:20 +0100

    Add check for cargo-web in build script

--- build.py
+++ build.py
@@ -37,47 +37,54 @@ webapp_parser.add_argument(
 
 check_parser = subparsers.add_parser("check", help="Check each feature combination")
 
-# parser.add_argument('--', dest="pass_args", nargs=argparse.REMAINDER)
-
 args = parser.parse_args()
 
 if args.action is None:
     parser.print_help()
     sys.exit(1)
 
-def call(cmd, wasm=False):
+def check_install_cargo_web():
+    if check_install_cargo_web.checked:
+        return
+
+    if subprocess.call(["cargo", "web", "-V"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) != 0:
+        cargo(["install", "cargo-web"])
+
+    check_install_cargo_web.checked = True
+
+check_install_cargo_web.checked = False
+
+def cargo(args):
+    cmd = ["cargo"] + args
+    print(" ".join(cmd))
+    subprocess.check_call(cmd)
+
+def cargo_web(args):
+    check_install_cargo_web()
+    cmd = ["cargo", "web"] + args + ["--target=wasm32-unknown-unknown"]
     print(" ".join(cmd))
     subprocess.check_call(cmd)
 
 def build_cli():
-    return call([
-        "cargo",
+    cargo([
         "run" if args.run else "build",
         "--features=cli",
         "--bin=mines-rs-cli"
     ])
 
 def build_webapp():
-    call([
-        "cargo",
-        "web",
+    cargo_web([
         "start" if args.run else "build",
         "--features=webapp",
-        "--target=wasm32-unknown-unknown",
         "--bin=mines-rs-webapp"
     ])
 
 def check_valid_features():
-    native_cmd = ["cargo", "check", "--no-default-features"]
+    for append_args in native_build_configs:
+        cargo(["check", "--no-default-features"] + append_args)
 
-    wasm_cmd = ["cargo", "web", "build", "--no-default-features", "--target=wasm32-unknown-unknown"]
-
-    for check_cmd, target_configs in [
-        (native_cmd, native_build_configs),
-        (wasm_cmd, wasm_build_configs)
-    ]:
-        for append_args in target_configs:
-            call(check_cmd + append_args)
+    for append_args in wasm_build_configs:
+        cargo_web(["build", "--no-default-features"] + append_args)
 
 if __name__ == "__main__":
     try:
